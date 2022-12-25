4b0315e1ff312155dc7fc7aa72e5d193cb91488b
Axel <111660292+RSVTAxelrpg@users.noreply.github.com>
2022-12-25 16:23:22 -0600

    Tipos de puntos con sus valores y sprites correspondientes

--- scripts/interactive-objects/point.gd
+++ scripts/interactive-objects/point.gd
@@ -1,6 +1,19 @@
 extends Area2D
 
+export var value = 1
+
+onready var sprite = $Sprite
+
+func _physics_process(delta):
+	if value == 1:
+		sprite.frame = 0
+	elif value == 2:
+		sprite.frame = 1
+
 func _on_point_body_entered(body):
 	if body.is_in_group("player"):
-		Gamehundler.puntos += 1
+		if value == 1:
+			Gamehundler.puntos += 1
+		elif value == 2:
+			Gamehundler.puntos += 3
 		queue_free()
