b21dcae186c6f12fd01c48a3eb94139cf1d20a44
Gustavo Cubilla <cubilla339@gmail.com>
2022-12-28 18:24:33 -0300

    Mejora, se agrego una escena de loza

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,7 +5,6 @@ var jum = -150
 var gravity = 300
 var velocity = Vector2()
 
-	
 func _input_button():
 	velocity.x = 0
 	
@@ -15,33 +14,14 @@ func _input_button():
 		velocity.x -= run
 	if (Input.is_action_just_pressed("ui_up")):
 		velocity.y = jum	
-	
-
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	_input_button()
-	
-
-	
 	velocity = move_and_slide(velocity, Vector2(0, -1))
 
-	
-	#var tile : Vector2 = tilemap.world_to_map(player.global_position)
-	#var id = tilemap.get_cell(int(tile.x), int(tile.y))
-	#if id != -1:
-		#tilemap.set_cell(int(tile.x), int(tile.y), -1)
 
-	
-
-
-
-
-func _on_Area2D_body_entered(body):
-		if body.is_in_group("well"):
-			#var nota = preload("res://scenes/tiles/floor.tscn")
-			#var notaa = PackedScene 
-			#nota.remove_tiles() 
-			var posi: Vector2 = $area/position.global_position
-			get_tree().call_group("well", "remove_tile", posi)
-			print("toco la cabeza", posi)
+func _on_area_body_entered(body):
+	if body.is_in_group("foor"):
+		var posi: Vector2 = $area/position.global_position
+		get_tree().call_group("well", "remove_tile", posi)
--- scripts/floor.gd
+++ scripts/floor.gd
@@ -6,6 +6,5 @@ func _ready():
 func remove_tile(pos):
 	var player_pos : Vector2 = world_to_map(pos)
 	set_cell(player_pos.x, player_pos.y, -1)
-	$AnimationPlayer.play("destruction")
-	TileSet.get_pause(true)
-	TileSet.get_pause(true)
+	
+
