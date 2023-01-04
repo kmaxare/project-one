a563d10db1b387d9f8f8a7b182563a5bc2c0e9bc
cubilla <cubilla339@gmail.com>
2023-01-04 20:46:16 -0300

    Agregando comentarios

--- scripts/floor.gd
+++ scripts/floor.gd
@@ -3,6 +3,7 @@ extends TileMap
 func _ready():
 	#remove_tile()
 	pass
+	#Eliminar tiles en prueba
 func remove_tile(pos):
 	var player_pos : Vector2 = world_to_map(pos)
 	set_cell(player_pos.x, player_pos.y, -1)
