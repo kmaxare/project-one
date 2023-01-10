a6484dd59f4c6ccc4ff313f727e3fccf023e867f
Axel <111660292+RSVTAxelrpg@users.noreply.github.com>
2023-01-09 21:34:44 -0600

    Limite superior y centrado de personaje a la izquierda en la camara

--- scripts/characters/player/Camera.gd
+++ scripts/characters/player/Camera.gd
@@ -5,5 +5,5 @@ onready var player = get_tree().get_nodes_in_group("player")[0]
 func _physics_process(delta):
 	position.y = player.position.y
 	
-	if position.x < player.position.x:
-		position.x = player.position.x
+	if player.position.x + 200 > position.x:
+		position.x = player.position.x + 200
