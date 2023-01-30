d699edf5b6fe200b0c808b7b27fc229aac71c8e0
Kevin Arenas <kevinarenas@unach.cl>
2023-01-30 18:04:29 -0500

    Agregando animacion de semilla

--- scripts/interactive-objects/point.gd
+++ scripts/interactive-objects/point.gd
@@ -1,14 +1,18 @@
 extends Area2D
 
 export var value = 1 # 0 = (+1 punto) / 1 = (+3 puntos)
+export (Texture) var semillaOne
+export (Texture) var semillaTwo
 
 onready var sprite = $Sprite
 
 func _ready():
 	if value == 1:
-		sprite.frame = 0
+		$Sprite.texture = semillaOne
 	elif value == 2:
-		sprite.frame = 1
+		$Sprite.texture = semillaTwo
+		
+	$AnimationPlayer.play("anim_idle")
 
 func _on_point_body_entered(body):
 	if body.is_in_group("player"):
@@ -16,4 +20,6 @@ func _on_point_body_entered(body):
 			Gamehundler.puntos += 1
 		elif value == 2:
 			Gamehundler.puntos += 3
+		$AnimationPlayer.play("anim_action")
+		yield($AnimationPlayer, "animation_finished")
 		queue_free()
