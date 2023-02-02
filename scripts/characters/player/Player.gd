11a6e0b0a24b95a64b9faffae4986639d56126fd
MiniGCJ23 <minigcj23@gail.com>
2023-02-02 17:19:31 -0600

    Agregando shake camara y border por editor

--- scripts/characters/player/Player.gd
+++ scripts/characters/player/Player.gd
@@ -4,24 +4,33 @@ extends KinematicBody2D
 const VectorUP = Vector2(0,-1);
 export var Vinicial = 30
 export var Vparo = 150 # 320
+#se llama al nodo camará del nodo player y  su nodo Tween
+onready var camera : Camera2D = $Camera2D
+onready var tween : Tween = $Camera2D/Tween
+#Variablle para generar numeros randoms 
+onready var rng = RandomNumberGenerator.new()
 
+# bandera que indica si el personaje esta mirando hacia la derecha
 var lookD = true
 var velocity = Vector2.ZERO
 # variables de la fuerza de salto y la gravedad del personaje
 var GRAVITY = 20
 var JUMPFORCE = -420 #-550
-var estado = "fall"
-var estadoant = "idle"
-var Vretroceso = 100
+var estado : String = "fall"
+var estadoant : String = "idle"
+var Vretroceso : float = 100
 var inhabilitado : bool = false
 var invunerable : bool = false
 var danado : bool = false
 var danadoD : bool = false #si es true el atacante esta a la derecha 
 
 func _ready():
-	pass
+	Gamehundler.puntos += 7 # para propositos de prueba
+	set_borders()
 
-func _physics_process(_delta):
+
+
+func _physics_process(_delta) -> void:
 	match estado:
 		"idle", "run":
 			move()
@@ -42,11 +51,11 @@ func _physics_process(_delta):
 	smash() #verifica si golpeo enemigo 
 
 func move():
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_d"):
 		velocity.x = min(velocity.x + Vinicial, Vparo)
 		#establece que mira ala derecha
 		lookD = true
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_a"):
 		velocity.x = max(velocity.x - Vinicial, -Vparo)
 		#establece que mira ala izquierda
 		lookD = false
@@ -58,11 +67,11 @@ func move():
 		estado = "run"
 
 func moversaltamdo():
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_d"):
 		velocity.x = min(velocity.x + Vinicial, Vparo)
 		#establece que mira ala derecha
 		lookD = true
-	elif Input.is_action_pressed("ui_left"):
+	elif Input.is_action_pressed("ui_a"):
 		velocity.x = max(velocity.x - Vinicial, -Vparo)
 		#establece que mira ala izquierda
 		lookD = false
@@ -77,7 +86,7 @@ func moversaltamdo():
 			estado = "fall"
 
 func saltar():
-	if Input.is_action_just_pressed("m") and is_on_floor():
+	if Input.is_action_just_pressed("ui_w") and is_on_floor():
 		velocity.y = JUMPFORCE
 		estado = "jump"
 func funcHurt():
@@ -108,6 +117,8 @@ func animacion():
 
 func damageReceived(damage, positionEnemy : Vector2):
 	if !invunerable:
+		#es el tiempo, potecia y prioridad del shake de camara
+		screen_shake(0.5, 0.3, 2)
 		danado = true
 		if global_position.x < positionEnemy.x:
 			danadoD = true
@@ -117,6 +128,9 @@ func damageReceived(damage, positionEnemy : Vector2):
 			Gamehundler.puntos -= damage
 		else:
 			Gamehundler.gameOver()
+			
+#se llama por medio del animation player cuando se termina la animacion
+#de hurt, pra marcar que el jugador vuelve a estar habilitado de usarse
 func postHurt():
 	inhabilitado = false
 
@@ -135,3 +149,49 @@ func smash() -> void:
 func _on_Timer_timeout():
 	invunerable = false
 	pass # Replace with function body.
+
+#Funcion para generar numeros randoms
+func random(minimo, maximo):
+	rng.randomize()
+	var random = rng.randf_range(minimo, maximo)
+	return random
+
+#se cambia el offset de la camara para sacudir la patalla
+func shake_camera(shake_pomer) -> void:
+	camera.offset_h = random(-shake_pomer, shake_pomer)
+	camera.offset_v = random(-shake_pomer, shake_pomer)
+	
+#se debe de aplicar tiemp de ejecución, intensidad de movimiento
+#y la prioridad de la sacudida de camará
+func screen_shake(shake_length : float, shake_power : float, shake_priority : int) -> void:
+	var current_shake_priority : int = 0
+	if shake_priority > current_shake_priority:
+		tween.interpolate_method(
+			self, #es el objeto a afectar
+			"shake_camera", #metodo o funcion afectada
+			shake_power, #valor inicial
+			0, #valor final, es 0 porque se regresa el offset al inicio
+			shake_length, #tiempo que transcurre entre uno y otro
+			Tween.TRANS_SINE, #transcision inicial
+			Tween.EASE_OUT #Transcision final 
+		)
+		tween.start()
+
+#Ajusta las esquinas de la camara con respectos a los nodos 
+#Position2D corner TL (Top Left) y cornerBR (Bottom Right)
+func set_borders() -> void:
+	#El if checa si existen estos nodos en la escena actual
+	if has_node("../settings/cornerTL") and has_node("../settings/cornerBR"):
+	#si existeen se ajustan las posiciones
+		var cornerTL : Position2D = get_node("../settings/cornerTL")
+		var cornerBR : Position2D = get_node("../settings/cornerBR")
+		camera.limit_bottom = cornerBR.global_position.y
+		camera.limit_right = cornerBR.global_position.x
+		camera.limit_top = cornerTL.global_position.y
+		camera.limit_left = cornerTL.global_position.x
+	else:
+		camera.limit_bottom = 10000
+		camera.limit_right = 10000
+		camera.limit_top = -1000
+		camera.limit_left = -1000
+		print("NO HAY NODO SETTINGS")
