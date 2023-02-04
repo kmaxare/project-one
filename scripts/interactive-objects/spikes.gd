93ad4a09665fb8bdc997bdf1a1362ebc181d2028
Kevin Arenas <kevinarenas@unach.cl>
2023-02-03 22:27:36 -0500

    Puliendo daño de spike

--- scripts/interactive-objects/spikes.gd
+++ scripts/interactive-objects/spikes.gd
@@ -1,6 +1,8 @@
 extends Area2D
 
+export var damage_spikes = 1
+
 
 func _on_spikes_body_entered(body):
 	if body.is_in_group("player"):
-		Gamehundler.puntos =- 1
+		body.damageReceived(damage_spikes, global_position)
