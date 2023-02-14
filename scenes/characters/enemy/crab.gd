e1c4f6c95b45600166d88fe1cc39a918d2bdba6a
Kevin Arenas <kevinarenas@unach.cl>
2023-02-13 20:55:08 -0500

    Laburando en las tareas del proyecto

--- scenes/characters/enemy/crab.gd
+++ scenes/characters/enemy/crab.gd
@@ -56,4 +56,4 @@ func set_direction_raycast() -> void:
 	
 func _on_Area2D_body_entered(body):
 	if body.is_in_group("player"):
-		body.damageReceived(damage, global_position)
+		body.damage_received(damage, global_position)
