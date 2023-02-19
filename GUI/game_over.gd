1db4d3e6487bb77fbc699e523f8109239bce454c
Axel Garcia <ar.pg@hotmail.com>
2023-02-19 00:28:22 -0600

    Escena de creditos

--- GUI/game_over.gd
+++ GUI/game_over.gd
@@ -1,23 +1,23 @@
-# Extiende la clase Control
 extends Control
 
-# Obtiene el nodo de texto para mostrar la puntuación del jugador
+# Se obtiene el nodo del botón Retry
+onready var button_retry = $VBoxContainer/HBoxContainer/Retry
+# Se obtiene el nodo de texto para mostrar la puntuación del jugador
 onready var score = $VBoxContainer/VBoxContainer2/Player/Score
 
 # Función llamada al terminar la carga del nodo
 func _ready():
 	# Establece el texto de la puntuación del jugador en el valor actual de la puntuación
 	score.text = str(GameHandler.points)
+	# Se establece el foco en el botón Retry para que sea seleccionado por defecto
+	button_retry.grab_focus()
 
-# Función llamada al presionar el botón de volver al menú principal
-func _on_Button_pressed():
-	# Cambia a la escena del menú principal
-	get_tree().change_scene("res://GUI/main_screen.tscn")
+# Función llamada cuando se presiona el botón Retry
+func _on_Retry_pressed():
+	if get_tree().change_scene("res://levels/level_one.tscn") != OK:
+		print("Error")
 
-
-func _on_return_menu_pressed():
-	pass # Replace with function body.
-
-
-func _on_retry_pressed():
-	pass # Replace with function body.
+# Función llamada cuando se presiona el botón Return
+func _on_Return_pressed():
+	if get_tree().change_scene("res://GUI/main_screen.tscn") != OK:
+		print("Error")
