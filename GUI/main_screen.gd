3eec175f57e6639a440c9f2b5c7837601a6bdddc
Axel Garcia <ar.pg@hotmail.com>
2023-02-25 17:56:34 -0600

    Ajustes al menú principal

--- GUI/main_screen.gd
+++ GUI/main_screen.gd
@@ -1,57 +1,65 @@
 extends Control
 
-#se carga el nodo del panel de introducción de nombre
+# Variables para controlar la música
+export (Texture) var music_on
+export (Texture) var music_off
+onready var music_button = $Music/MusicButton
+onready var music_player = $Music/MusicPlayer
+var is_music_playing = true
+
+# Variables para el panel de introducción del nombre del jugador
+onready var start_button = $VBoxContainer/Start
 onready var name_panel : Panel = $NamePanel
-#se carga la propiedad text del nodo name_intro
-onready var name_player : String = $NamePanel/NameIntro.text
+onready var name_player = $NamePanel/NameIntro
 
-#función que se ejecuta al principio de ejecutarse la escena
 func _ready() -> void:
-	#oculta el panel de introducir nombre
+	# Al cargar el nodo, se oculta el panel y se pone el foco en el botón de inicio
 	name_panel.hide()
+	start_button.grab_focus()
 
-#Función que se activa cuando se presiona el botón "start"
-func _on_start_pressed() -> void:
-	name_panel.show()
-
-#Función que se activa cuando se presiona el botón "score"
-func _on_score_pressed() -> void:
-	#Cambia a la escena "score.tscn"
-	if get_tree().change_scene("res://GUI/score.tscn") != OK:
-		print('Error para reconocer escena score')
-#		get_tree().change_scene("res://GUI/score.tscn")
-
-#Función que se activa cuando se presiona el botón "instruccions"
-func _on_instruccions_pressed() -> void:
-	#Cambia a la escena "instruccions.tscn"
-	if get_tree().change_scene("res://GUI/instruccions.tscn") != OK:
-		print('Error para reconocer escena instruccions')
-
-#Función que se activa cuando se presiona el botón "controls"
-func _on_controls_pressed() -> void:
-	#Cambia a la escena "controls.tscn"
-	if get_tree().change_scene("res://GUI/controls.tscn") != OK:
-		print('Error para reconocer escena controls')
-
-#Función que se activa cuando se presiona el botón "credits"
-func _on_credits_pressed() -> void:
-	#Cambia a la escena "credits.tscn"
+# Botón para ir a la pantalla de créditos
+func _on_Credits_pressed():
 	if get_tree().change_scene("res://GUI/credits.tscn") != OK:
 		print('Error para reconocer escena credits')
 
-#Función que se activa cuando se presiona el botón "quit"
-func _on_quit_pressed() -> void:
-	#Cierra el programa
+# Botón para mostrar las instrucciones y controles del juego
+func _on_InstruccionsControls_pressed():
+	pass # Replace with function body.
+	
+# Botón para controlar el volumen de la música
+func _on_MusicButton_pressed():
+	if is_music_playing:
+		# Si la música está sonando, se detiene y se cambia la textura del botón
+		is_music_playing = false
+		music_button.texture_normal = music_off
+		music_player.volume_db = -80
+	else:
+		# Si la música está detenida, se reproduce y se cambia la textura del botón
+		is_music_playing = true
+		music_button.texture_normal = music_on
+		music_player.volume_db = -10
+	
+# Botón para guardar el nombre del jugador y comenzar el juego
+func _on_NameDone_pressed():
+	game_handler.player_name = name_player.text
+	if get_tree().change_scene("res://levels/level_one.tscn") != OK:
+		print("Error para reconocer la escena del nivel")
+		
+# Botón para cancelar la introducción del nombre del jugador y volver al menú principal
+func _on_NameReturn_pressed():
+	name_panel.hide()
+	start_button.grab_focus()
+
+# Botón para salir del juego
+func _on_Quit_pressed():
 	get_tree().quit()
 
-# Función que se activa cuando se presiona el botón OK
-# de cuando se introduce el juego
-func _on_name_done_pressed() -> void:
-	game_handler.player_name = name_player
-	if get_tree().change_scene("res://levels/level_one.tscn") != OK:
-		print('Error para reconocer escena level_one')
+# Botón para ir a la pantalla de puntuación
+func _on_Score_pressed():
+	if get_tree().change_scene("res://GUI/score.tscn") != OK:
+		print('Error para reconocer escena score')
 
-# se presiono el boton regresar del panel de introducir nombre
-func _on_panel_hide_pressed() -> void:
-	name_panel.hide()
-	
+# Botón para empezar a jugar, se muestra el panel de introducción del nombre
+func _on_Start_pressed():
+	name_panel.show()
+	name_player.grab_focus()
